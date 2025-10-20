b55b63a9e48fa38a72bd837c0e2a37bc58c0a384
Kazuma Arino <hogeika2@gmail.com>
2025-10-20 10:44:47 +0900

    Rename first, second to strong, weak.

--- android/src/main/java/io/github/karino2/paoogo/goengine/HybridEngine.kt
+++ android/src/main/java/io/github/karino2/paoogo/goengine/HybridEngine.kt
@@ -2,19 +2,19 @@ package io.github.karino2.paoogo.goengine
 
 
 class Policy(val threshold : Int) {
-    fun isFirst(te: Int) : Boolean {
+    fun isStrong(te: Int) : Boolean {
         return te < threshold
     }
 }
 
-class HybridEngine(val first: GoEngine, val second: GoEngine, val policy: Policy) : GoEngine {
+class HybridEngine(private val strong: GoEngine, private val weak: GoEngine, val policy: Policy) : GoEngine {
     var te = 0
     override fun genMoveInternal(isBlack: Boolean): Int {
         te += 1
-        val (main, sub) =  if(policy.isFirst(te)) {
-            Pair(first, second)
+        val (main, sub) =  if(policy.isStrong(te)) {
+            Pair(strong, weak)
         } else {
-            Pair(second, first)
+            Pair(weak, strong)
         }
         val res = main.genMoveInternal(isBlack)
         val pos = GoEngine.internalToPos(res)
@@ -26,35 +26,35 @@ class HybridEngine(val first: GoEngine, val second: GoEngine, val policy: Policy
     }
 
     override fun debugInfo(): String? {
-        return "te=${te}, isFirst=${policy.isFirst(te)}"
+        return "te=${te}, isFirst=${policy.isStrong(te)}"
     }
 
     override fun setKomi(komi: Float) {
-        first.setKomi(komi)
-        second.setKomi(komi)
+        strong.setKomi(komi)
+        weak.setKomi(komi)
     }
 
     override fun clearBoard() {
-        first.clearBoard()
-        second.clearBoard()
+        strong.clearBoard()
+        weak.clearBoard()
         te = 0
     }
 
     override fun setBoardSize(size: Int) {
-        first.setBoardSize(size)
-        second.setBoardSize(size)
+        strong.setBoardSize(size)
+        weak.setBoardSize(size)
     }
 
     override fun doMove(x: Int, y: Int, isBlack: Boolean): Boolean {
         te += 1
-        first.doMove(x, y, isBlack)
-        return second.doMove(x, y, isBlack)
+        strong.doMove(x, y, isBlack)
+        return weak.doMove(x, y, isBlack)
     }
 
     override fun doPass(isBlack: Boolean) {
         te += 1
-        first.doPass(isBlack)
-        second.doPass(isBlack)
+        strong.doPass(isBlack)
+        weak.doPass(isBlack)
     }
 
 }
