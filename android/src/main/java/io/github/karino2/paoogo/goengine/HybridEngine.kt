082b5af0f04054c1c884f8138d2e7f0e8d704fab
Kazuma Arino <hogeika2@gmail.com>
2025-10-23 17:28:23 +0900

    Add a little stronger hybrid engine.

--- android/src/main/java/io/github/karino2/paoogo/goengine/HybridEngine.kt
+++ android/src/main/java/io/github/karino2/paoogo/goengine/HybridEngine.kt
@@ -1,17 +1,78 @@
 package io.github.karino2.paoogo.goengine
 
+import android.util.Log
+import kotlin.math.abs
+import kotlin.math.pow
+import kotlin.random.Random
 
-class Policy(val threshold : Int) {
-    fun isStrong(te: Int) : Boolean {
-        return te < threshold
+
+class Policy(val level : Int) {
+    /*
+       policy is adjusted for gnugo2.8 and liberty.
+       For opening, liberty is too weak, so use stronger one.
+
+       In the middle, we want to use liberty.
+       For end game, we want to use gnugo2.8 again.
+
+       So the ideal value of isStrong prob is:
+
+       |<---- opening -->!<-- middle -- -->|<-- end game -->|
+       1.0, 1.0 ..., 1.0, 0.0, 0.0 ..., 0.0, 1.0, 1.0, 1.0...
+
+       But when middle end eng game begins is hard to know, so interpolate between 0.0 and 1.0,
+       use that prob to randomize result.
+
+       We have two guessed number:
+
+       END_OF_OPENING (about 14 te in 9x9)
+       MIDDLE
+
+       MIDDLE is the peak of weaker engine prob.
+       And We define the start of endgame as the point opposite to init, centered on middle.
+       BEGIN_OF_ENDGAME = 2*MIDDLE - END_OF_OPENING
+       After BEGIN_OF_ENDGAME, the prob becomes 1.0
+     */
+    private fun weakProb(te: Int, total: Int) : Double{
+        // 14
+        val endOfOpening = (total*14)/81
+
+        val middle = when(level) {
+            1-> total*2/3
+            2-> total/2
+            else-> total/3
+        }
+
+        val w = abs(middle - endOfOpening).toDouble()
+        val beginEndGame = 2*middle - endOfOpening
+        return if (te < endOfOpening) {
+            0.0 // strong
+        } else if (middle-te > 0) {
+            /* interpolation from 1.0 to 0.5 toward middle, concave */
+            1.0 - 0.5*(te-endOfOpening).toDouble().pow(2.0)/w.pow(2.0)
+        } else if(te > beginEndGame) {
+            0.0 // strong
+        } else {
+            /* interpolation from 0.5 to 0.0 toward beginEndGame, concave */
+            0.5*(te-beginEndGame).toDouble().pow(2.0)/w.pow(2.0)
+        }
+    }
+    fun isStrong(te: Int, boardSize: Int) : Boolean {
+        val pval = weakProb(te, boardSize*boardSize)
+        val isStrong = Random.nextDouble() > pval
+        Log.d("PaooGo", "te=${te}, prob=${pval}, isStrong=${isStrong}")
+
+        return isStrong
     }
 }
 
 class HybridEngine(private val strong: GoEngine, private val weak: GoEngine, val policy: Policy) : GoEngine {
-    var te = 0
+    private var te = 0
+    private var boardSize = 9
+    private var lastStrong = true
     override fun genMoveInternal(isBlack: Boolean): Int {
         te += 1
-        val (main, sub) =  if(policy.isStrong(te)) {
+        lastStrong = policy.isStrong(te, boardSize)
+        val (main, sub) =  if(lastStrong) {
             Pair(strong, weak)
         } else {
             Pair(weak, strong)
@@ -26,7 +87,7 @@ class HybridEngine(private val strong: GoEngine, private val weak: GoEngine, val
     }
 
     override fun debugInfo(): String? {
-        return "te=${te}, isFirst=${policy.isStrong(te)}"
+        return "te=${te}, lastStrong=${lastStrong}"
     }
 
     override fun setKomi(komi: Float) {
@@ -43,6 +104,7 @@ class HybridEngine(private val strong: GoEngine, private val weak: GoEngine, val
     override fun setBoardSize(size: Int) {
         strong.setBoardSize(size)
         weak.setBoardSize(size)
+        boardSize = size
     }
 
     override fun doMove(x: Int, y: Int, isBlack: Boolean): Boolean {
